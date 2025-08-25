a0c2aee89eee112fa47092dc4542cc6d2c14ec39
omid <omid@omids-MacBook-Pro.local>
2025-08-25 18:22:08 +0330

    fixed NodeJs http Module for example ....

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-//first Nodejs Http Module
+// first Nodejs Http Module
 const http = require("http");
 const PORT = 3000;
-const server = http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Hello World!",
-      })
-    );
-  })
-  .addListener(PORT, () => {
-    console.log(`click on Port 3000 :localhost:http://${PORT}`);
-  });
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
