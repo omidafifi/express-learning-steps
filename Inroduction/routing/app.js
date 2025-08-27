f822fa8136a14628be4e59fae7f1b86e7cecfa4c
omidafifi <afifiomid@yahoo.com>
2025-08-27 15:37:01 +0330

    app.get

--- Inroduction/routing/app.js
+++ Inroduction/routing/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
+const PORT = 3000;
 // تعریف یک روت ساده
-app.get("/", (req, res) => {
-  console.log("Welcome to route /");
-  res.send("<h1>Hello Express.js 🚀</h1>");
-});
+// app.get("/", (req, res) => {
+//   console.log("Welcome to route /");
+//   res.send("<h1>Hello Express.js 🚀</h1>");
+// });
 
 // مثال JSON
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from JSON endpoint!" });
+// app.get("/api", (req, res) => {
+//   res.json({ message: "Hello from JSON endpoint!" });
+
+// GET /users
+app.get("/users", (req, res) => {
+  res.json({
+    users: [
+      { id: 1, name: "user1" },
+      { id: 2, name: "user2" },
+      { id: 3, name: "user3" },
+      { id: 4, name: "user4" },
+    ],
+  });
 });
 
 // اجرای سرور
