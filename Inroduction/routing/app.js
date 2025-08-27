6c9cc5e06beea88fe9298d9b6d49b9f7e587de81
omid <omid@omids-MacBook-Pro.local>
2025-08-27 13:55:53 +0330

    first exmaple routing app in express

--- Inroduction/routing/app.js
+++ Inroduction/routing/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+  console.log("wellcom to rouat...");
+  // res.send("<h1>Hello expressJS .....</h1>");
+  // res.send({ message: "Hello (Json)" });
+  res.send("hello express");
+});
 app.listen(3000, () => {
-  console.log("server run on port 3000");
+  console.log("Server is running on port 3000 at http://localhost:3000");
 });
