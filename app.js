cce5e2ba187534c7978a0d6c1b1973154da91932
omidafifi <afifiomid@yahoo.com>
2026-05-02 18:57:06 +0330

    Implement course retrieval functionality and restructure routes

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const express = require("express");
 const PORT = 3000;
 const app = express(); //()
 
-app.get("/", (req, res) => {
-  res.send("home page");
+
+app.get("/courses/:id", (req, res) => {
+  const findCourse = courses.find(
+    (course) => course.id === Number(req.params.id),
+  );
+
+  if (!findCourse) {
+    res.status(404).send("دوره مورد نظر یافت نشد.");
+  }
+
+  res.send(findCourse);
 });
 
-app.get("/about", (req, res) => {
-  res.send("home about");
-});
-app.get("/articles", (req, res) => {
-  res.send("home articles");
-});
-app.get("/user", (req, res) => {
-  res.send("home user");
-});
+// app.get("/about", (req, res) => {
+//   res.send("home about");
+// });
+// app.get("/articles", (req, res) => {
+//   res.send("home articles");
+// });
+// app.get("/user", (req, res) => {
+//   res.send("home user");
+// });
 
 app.listen(PORT, () => {
   console.log("app run on Port 3000");
---
+++ src/models/courseModel.js
@@ -0,0 +1,26 @@
+const courses = [
+  {
+    id: 1,
+    courseName: "A1",
+    price: "3_500_000",
+  },
+  {
+    id: 2,
+    courseName: "A2",
+    price: "4_500_000",
+  },
+  {
+    id: 3,
+    courseName: "B1",
+    price: "7_500_000",
+  },
+  {
+    id: 4,
+    courseName: "B2",
+    price: "10_000_000",
+  },
+];
+
+module.exports = {
+  courses,
+};
