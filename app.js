3d88cd9cfc46f2dc115480512cd01a8c7616e394
omidafifi <afifiomid@yahoo.com>
2026-05-07 12:07:07 +0330

    Refactor app.js: clean up commented code and improve formatting

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
-const courseRoutes = require("./src/routes/course_Routes"); // وارد کردن مسیرها
+// const courseRoutes = require("./src/routes/course_Routes"); // وارد کردن مسیرها
 
 app.use(express.json());
+app.use(express.urlencoded())
 
-// استفاده از مسیرهای مربوط به دوره‌ها
-// تمام مسیرهایی که در فایل courseRoutes هستند، با پیش‌وند /courses شروع می‌شوند
-app.use("/courses", courseRoutes);
+// app.use("/courses", courseRoutes);
 
-app.post("/courses/users", (req, res) => {
+app.post("/courses/users", (req, res ) => {
   // همه داده‌ها را در یک قالب واحد جمع می‌کنیم
   const responseData = {
     Course: req.body.Course,
@@ -19,6 +18,7 @@ app.post("/courses/users", (req, res) => {
   // فقط یک بار پاسخ را ارسال می‌کنیم
   res.status(200).json(responseData);
 });
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
